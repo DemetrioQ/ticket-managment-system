81ec0e37a18a7e39fbfbbeaabebffcf6d26521d9
Demetrio Quiñones <demetriorqe@gmail.com>
2021-11-24 01:03:29 -0400

    Formatted document

--- server/models/priority.js
+++ server/models/priority.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Priority extends Model {
     /**
@@ -12,12 +10,15 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Priority.init({
-    priority: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Priority',
-  });
+  }
+  Priority.init(
+    {
+      priority: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Priority',
+    }
+  );
   return Priority;
 };
